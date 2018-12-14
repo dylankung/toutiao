e1a0766ca843e9e22f4d3f7af2b866eef14a3759
delronkung <delron.con@gmail.com>
2018-12-14 17:58:08 +0800

    fix report bug

--- common/models/news.py
+++ common/models/news.py
@@ -148,7 +148,7 @@ class Report(db.Model):
     user_id = db.Column(db.Integer, doc='用户ID')
     article_id = db.Column(db.Integer, doc='文章ID')
     type = db.Column(db.Boolean, doc='问题类型')
-    remark = db.Column(db.String, default='', doc='备注问题')
+    remark = db.Column(db.String, doc='备注问题')
     ctime = db.Column('create_time', db.DateTime, default=datetime.now, doc='创建时间')
 
 
