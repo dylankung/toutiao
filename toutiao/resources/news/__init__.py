422a2fe4a5260107b4c2aeb4169dee8670e9df95
delronkung <delron.con@gmail.com>
2019-01-24 16:04:19 +0800

    remove get article list v1.0 api

--- toutiao/resources/news/__init__.py
+++ toutiao/resources/news/__init__.py
@@ -13,8 +13,8 @@ news_api.representation('application/json')(output_json)
 news_api.add_resource(article.ArticleResource, '/v1_0/articles/<int(min=1):article_id>',
                       endpoint='Article')
 
-news_api.add_resource(article.ArticleListResource, '/v1_0/articles',
-                      endpoint='Articles')
+# news_api.add_resource(article.ArticleListResource, '/v1_0/articles',
+#                       endpoint='Articles')
 
 news_api.add_resource(article.ArticleListResourceV1D1, '/v1_1/articles',
                       endpoint='ArticlesV1_1')
