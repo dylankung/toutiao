a95c9eaf4a4736f8f5d2c9a68333c96517d0d8e7
delronkung <delron.con@gmail.com>
2019-01-24 11:22:37 +0800

    fix bug mp return with no user profile photo

--- mp/resources/user/passport.py
+++ mp/resources/user/passport.py
@@ -131,7 +131,7 @@ class AuthorizationResource(Resource):
                 'refresh_token': refresh_token,
                 'id': user.id,
                 'name': user.name,
-                'photo': current_app.config['QINIU_DOMAIN'] + user.profile_photo}, 201
+                'photo': (current_app.config['QINIU_DOMAIN'] + user.profile_photo) if user.profile_photo else ''}, 201
 
     def put(self):
         """
