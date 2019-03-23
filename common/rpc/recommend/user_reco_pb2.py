ab6403ce887f5cc851ad07b6236c9f2af2212e05
delronkung <delron.con@gmail.com>
2019-03-23 11:24:49 +0800

    update grpc proto to int64

--- common/rpc/recommend/user_reco_pb2.py
+++ common/rpc/recommend/user_reco_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x0fuser_reco.proto\"T\n\x04User\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x12\n\nchannel_id\x18\x02 \x01(\x05\x12\x13\n\x0b\x61rticle_num\x18\x03 \x01(\x05\x12\x12\n\ntime_stamp\x18\x04 \x01(\x03\"2\n\x07\x41rticle\x12\x12\n\narticle_id\x18\x01 \x01(\x05\x12\x13\n\x0b\x61rticle_num\x18\x02 \x01(\x05\"E\n\x06param2\x12\r\n\x05\x63lick\x18\x01 \x01(\t\x12\x0f\n\x07\x63ollect\x18\x02 \x01(\t\x12\r\n\x05share\x18\x03 \x01(\t\x12\x0c\n\x04read\x18\x04 \x01(\t\"5\n\x06param1\x12\x12\n\narticle_id\x18\x01 \x01(\x05\x12\x17\n\x06params\x18\x02 \x01(\x0b\x32\x07.param2\"J\n\x05Track\x12\x10\n\x08\x65xposure\x18\x01 \x01(\t\x12\x1b\n\nrecommends\x18\x02 \x03(\x0b\x32\x07.param1\x12\x12\n\ntime_stamp\x18\x03 \x01(\x03\"\x1d\n\x07Similar\x12\x12\n\narticle_id\x18\x01 \x03(\x05\x32]\n\rUserRecommend\x12!\n\x0euser_recommend\x12\x05.User\x1a\x06.Track\"\x00\x12)\n\x11\x61rticle_recommend\x12\x08.Article\x1a\x08.Similar\"\x00\x62\x06proto3')
+  serialized_pb=_b('\n\x0fuser_reco.proto\"T\n\x04User\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x12\n\nchannel_id\x18\x02 \x01(\x05\x12\x13\n\x0b\x61rticle_num\x18\x03 \x01(\x05\x12\x12\n\ntime_stamp\x18\x04 \x01(\x03\"2\n\x07\x41rticle\x12\x12\n\narticle_id\x18\x01 \x01(\x03\x12\x13\n\x0b\x61rticle_num\x18\x02 \x01(\x05\"E\n\x06param2\x12\r\n\x05\x63lick\x18\x01 \x01(\t\x12\x0f\n\x07\x63ollect\x18\x02 \x01(\t\x12\r\n\x05share\x18\x03 \x01(\t\x12\x0c\n\x04read\x18\x04 \x01(\t\"5\n\x06param1\x12\x12\n\narticle_id\x18\x01 \x01(\x03\x12\x17\n\x06params\x18\x02 \x01(\x0b\x32\x07.param2\"J\n\x05Track\x12\x10\n\x08\x65xposure\x18\x01 \x01(\t\x12\x1b\n\nrecommends\x18\x02 \x03(\x0b\x32\x07.param1\x12\x12\n\ntime_stamp\x18\x03 \x01(\x03\"\x1d\n\x07Similar\x12\x12\n\narticle_id\x18\x01 \x03(\x03\x32]\n\rUserRecommend\x12!\n\x0euser_recommend\x12\x05.User\x1a\x06.Track\"\x00\x12)\n\x11\x61rticle_recommend\x12\x08.Article\x1a\x08.Similar\"\x00\x62\x06proto3')
 )
 
 
@@ -86,7 +86,7 @@ _ARTICLE = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='article_id', full_name='Article.article_id', index=0,
-      number=1, type=5, cpp_type=1, label=1,
+      number=1, type=3, cpp_type=2, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -176,7 +176,7 @@ _PARAM1 = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='article_id', full_name='param1.article_id', index=0,
-      number=1, type=5, cpp_type=1, label=1,
+      number=1, type=3, cpp_type=2, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -259,7 +259,7 @@ _SIMILAR = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='article_id', full_name='Similar.article_id', index=0,
-      number=1, type=5, cpp_type=1, label=3,
+      number=1, type=3, cpp_type=2, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
