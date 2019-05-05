80e2037bb51397c893c8ccb23d9b1bef827e68f3
delronkung <delron.con@gmail.com>
2019-05-05 12:56:33 +0800

    fix return user article attitude in collection and history api

--- toutiao/resources/news/reading.py
+++ toutiao/resources/news/reading.py
@@ -44,7 +44,7 @@ class ReadingHistoryListResource(Resource):
         user_article_attitude_cache = cache_user.UserArticleAttitudeCache(user_id)
         for article_id in article_ids:
             article = cache_article.ArticleInfoCache(int(article_id)).get()
-            article['is_liking'] = user_article_attitude_cache.determine_liking_article(article_id)
+            article['is_liking'] = user_article_attitude_cache.determine_liking_article(int(article_id))
             results.append(article)
 
         return {'total_count': total_count, 'page': page, 'per_page': per_page, 'results': results}
